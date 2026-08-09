2f87564238e4115bfe8c8af283bc2ed4ea7619a8
Oluwagbenga Oyewole <gbengaoyewole89@gmail.com>
2026-08-09 13:59:16 +0100

    feat: added terraform modules/serverless directory

---
+++ terraform/modules/serverless/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name"  { type = string }
+variable "project_tag"   { type = string }
+variable "environment"   { type = string }
+variable "student_id"    { type = string }
+variable "cluster_name"  { type = string }
